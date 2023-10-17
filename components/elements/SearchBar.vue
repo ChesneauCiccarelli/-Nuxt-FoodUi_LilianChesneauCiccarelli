c2419459f294956c1dc0457b0f4e0930dee9a75a
Lilian <chci.lilian@gmail.com>
2023-10-17 16:42:02 +0200

    feat : Searchbar icons and changes to card button

--- components/elements/SearchBar.vue
+++ components/elements/SearchBar.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import { useGlobalStore } from "@/stores/global";
+const store = useGlobalStore();
+</script>
 
 <template>
   <div class="searchbar searchbar__research">
@@ -9,11 +12,17 @@
     <div class="searchbar__icon">
       <IconsSet variant="Default" name="Cart" size="Small" />
     </div>
+    <div class="count">
+      {{ store.cart.length }}
+      {{ store.cart }}
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .searchbar {
+  position: relative;
+
   &__left {
     align-items: center;
     display: flex;
@@ -50,5 +59,18 @@
     padding-left: rem(10);
     border-left: solid 1px $gray;
   }
+}
+
+.count {
+  background-color: $primary-color;
+  border-radius: 100%;
+  color: $white;
+  filter: drop-shadow(0px 0px 22px rgba(204, 0, 0, 0.8));
+  padding: 3px 5px;
+  position: absolute;
+  top: 20%;
+  right: 5%;
+
+  font-size: $small-font-size;
 }
 </style>
